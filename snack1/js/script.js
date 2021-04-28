f93702d3c093b8a8106da99f0c866b1ca7f7e3af
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-28 15:01:53 +0200

    prova per vedere se funziona

---
+++ snack1/js/script.js
@@ -0,0 +1,14 @@
+let studente = {
+    nome: 'alessandro',
+    cognome: 'ciancio',
+    age: 41,
+}
+
+
+
+//console.log(studente.nome);
+//console.log(studente.cognome);
+
+
+
+console.log(studente);
