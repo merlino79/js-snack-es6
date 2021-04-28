7c1e2666034f4f47f29af06258db392e6aa8c74b
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-28 15:38:41 +0200

    prova ciclo for of

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,18 +1,100 @@
-let studente = [{
-        nome: 'alessandro',
-        cognome: 'ciancio',
-        age: 41,
+//array di 10 ogg rappr zucchine varietà e peso e lunghezza
+//calcolo peso di tutte le zucchine
+
+let zucchine = [{
+
+
+        origine: 'Nera di Milano',
+        lunghezza: '20 cm',
+        peso: 3.75,
+
+    },
+
+    {
+        origine: 'Alberello',
+        lunghezza: '45 cm',
+
+        peso: 15.70,
+
     },
     {
-        nome: 'marco',
-        cognome: 'bevolacqua',
-        age: 35,
+        origine: 'Tonda di Nizza',
+        lunghezza: "35 cm",
+
+
+        peso: 25,
+
+
+    },
+    {
+        origine: 'Lunghe fiorentine',
+        lunghezza: '33 cm',
+
+
+        peso: 22.08,
+
+
+    },
+    {
+        origine: 'Trombetta di_Albenga',
+        lunghezza: "5 cm",
+
+
+        peso: 12.70,
+
+
+    },
+    {
+        origine: 'Trombetta di_Albenga',
+        lunghezza: "15 cm",
+
+
+        peso: 7,
+
+
+    },
+    {
+        origine: 'Zucchino giallo Golden',
+        lunghezza: "15,7 cm",
+
+
+        peso: 45,
+
+
+    },
+    {
+        origine: 'Italiane',
+        lunghezza: "22 cm",
+
+
+        peso: 25.3,
+
+
+    },
+    {
+        origine: 'Francesi',
+        lunghezza: "28 cm",
+
+
+        peso: 30.4,
+
+
+    },
+    {
+        origine: 'Ungheresi',
+        lunghezza: '45 cm',
+
+
+        peso: 45,
+
+
     },
 
+
+
+
 ];
-
-for (datiStudenti of studente) {
-    const studenti = datiStudenti;
-
-    console.log(studenti.nome + " " + studenti.cognome);
+console.log(zucchine);
+for (zucchini of zucchine) {
+    console.log('peso zucchina ' + zucchini.peso);
 }
