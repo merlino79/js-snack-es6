c79f8bd0bebc345d10458b10d9842ba878e70b9e
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-28 15:19:06 +0200

    prova

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,14 +1,18 @@
-let studente = {
-    nome: 'alessandro',
-    cognome: 'ciancio',
-    age: 41,
+let studente = [{
+        nome: 'alessandro',
+        cognome: 'ciancio',
+        age: 41,
+    },
+    {
+        nome: 'marco',
+        cognome: 'bevolacqua',
+        age: 35,
+    },
+
+];
+
+for (datiStudenti of studente) {
+    const studenti = datiStudenti;
+
+    console.log(studenti.nome + " " + studenti.cognome);
 }
-
-
-
-//console.log(studente.nome);
-//console.log(studente.cognome);
-
-
-
-console.log(studente);
