ca3c5e5329ea408ad71cee2bbfcd74cb6c5407e9
Merlino79 <alessandro.ciancio@hotmail.com>
2021-05-03 20:23:17 +0200

    inizio

---
+++ array oggetti e filter/asset/js/script.js
@@ -0,0 +1,47 @@
+// Crea un array di oggetti che rappresentano degli animali: ogni animale ha un nome, una famiglia e una classe. Es: 
+// [
+// 	{ nome: ‘leone’, famiglia: ‘felidi’, classe: ‘mammiferi’ },
+// 	{ nome: ‘cane’, ’famiglia: ‘canidi’, classe: ‘mammiferi’ },
+// 	{ nome: ‘gallina’, famiglia: ‘fasianidi’, classe: ‘uccelli’ },
+
+
+
+$(function() {
+    const animals = [{
+            nome: 'leone',
+            famiglia: 'felidi',
+            classe: 'mammiferi'
+        },
+        {
+            nome: 'tigre',
+            famiglia: 'felidi',
+            classe: 'mammiferi'
+        },
+        {
+            nome: 'gallina',
+            famiglia: 'fasianidi',
+            classe: 'uccello'
+        },
+
+
+    ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
