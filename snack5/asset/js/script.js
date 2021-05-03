6c0f63f3d390af0a16af0aeae9a1e6a967233b5d
Merlino79 <alessandro.ciancio@hotmail.com>
2021-05-03 18:53:59 +0200

    creazione array

--- snack5/asset/js/script.js
+++ snack5/asset/js/script.js
@@ -1,4 +1,14 @@
+// Esercizio (map) -> si potrebbe ottimizzare con una funzione separata per il capitalize
+// A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+// Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]
+
+
+
+
 $(function() {
+    let arr = ['pippo', 'PLUTO', 'Paperino']
+
+    console.log(arr)
 
 
 
